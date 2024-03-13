630976eeb6adca3439ebdbb92194d1024280648a
Expua1 <badsnake1@gmail.com>
2024-03-14 00:17:22 +0200

    init

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <button>toggle</button>
+            <button onClick={onToggleButton}>toggle</button>
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
