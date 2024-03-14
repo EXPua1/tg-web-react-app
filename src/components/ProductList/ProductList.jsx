d819964aeff9306c6edb5669ea6f045d0aff9fd5
Expua1 <badsnake1@gmail.com>
2024-03-14 03:56:24 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './ProductList.css'
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
@@ -22,7 +22,33 @@ const getTotalPrice = (items) => {   //Считаем стоимать това�
 }
 const ProductList = () => {
     const[addedItems, setAddedItems] = useState([]);
-    const {tg} = useTelegram()
+    const {tg, queryId} = useTelegram()
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+
+
+
+        }
+       fetch('http://localhost:8000',{
+           method: 'POST',
+           headers: {
+               'Content-Type' : 'application/json',
+           },
+           body: JSON.stringify(data)
+       })
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData]);
+
     const onAdd =(product) =>{
         const alreadyAdded = addedItems.find(item => item.id ===product.id);
         let newItems =[];
