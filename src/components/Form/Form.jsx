1e2a57b05b5da63e2e6b57c1dc9b5b9f7cb94994
Expua1 <badsnake1@gmail.com>
2024-03-14 02:17:59 +0200

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,8 +15,14 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [country, street, subject])
+    }, [])
 
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, []);
 
 
     useEffect(() => {
