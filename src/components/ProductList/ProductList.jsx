b8181770eae52a0b6bd430a11a627c8f15e50fdb
Expua1 <badsnake1@gmail.com>
2024-03-15 16:46:13 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             queryId: queryId,
         };
 
-        fetch('http://34.168.57.4/web-data:8000', {
+        fetch('http://34.168.57.4:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json', // Используем application/json для JSON данных
