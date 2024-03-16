e1e21d37e5b69a726650cefcf1ab02ffe855e032
Expua1 <badsnake1@gmail.com>
2024-03-16 21:53:43 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -33,7 +33,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://34.125.61.101', {
+        fetch('https://tg.kitzen.dev/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
