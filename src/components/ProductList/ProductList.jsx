459a1e2dab35ef51f4fd202eeccfe3afa3a745aa
Expua1 <badsnake1@gmail.com>
2024-03-17 16:54:51 +0200

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,13 +6,13 @@ import {useCallback, useEffect} from "react";
 
 const products = [
     {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые', image:`jeans.png`},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая', image:`jeans.png`},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые', image:`jeans.png`},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая', image:`jeans.png`},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые', image:`jeans.png`},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая', image:`jeans.png`},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые', image:`jeans.png`},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая', image:`jeans.png`},
 ]
 
 const getTotalPrice = (items = []) => {
