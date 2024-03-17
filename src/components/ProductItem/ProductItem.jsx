81f868f254a18c79060b1fa5e64b018fd83b887a
Expua1 <badsnake1@gmail.com>
2024-03-17 11:50:42 +0200

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
+            <div className={'img'} style={{backgroundImage: `url(${process.env.PUBLIC_URL}/${product.image})`}}/>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}/>
